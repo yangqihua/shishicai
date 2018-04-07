a3aa88fcd42a9af598ba2460e23df093ca2d898c
yangqihua <904693433@qq.com>
2018-04-07 17:34:20 +0800

    先只实验冠军

--- application/api/controller/cai/Pk10.php
+++ application/api/controller/cai/Pk10.php
@@ -100,7 +100,8 @@ class Pk10 extends Api
         $old = $resultModel->where(['qihao' => $item['qihao']])->find();
         if (!$old) {
             foreach ($this->huiheMap as $key => $value) {
-                if ($key != 'total') {
+//                if ($key != 'total') {
+                if ($key == 'yi') {
                     $this->checkHuihe($item, $key);
                 }
             }
