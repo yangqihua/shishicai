94b03a82a7270bf7c7679aa423561d004fe8a112
yangqihua <904693433@qq.com>
2018-04-11 11:57:18 +0800

    跨域请求

--- public/index.php
+++ public/index.php
@@ -19,6 +19,6 @@ if (!is_file(APP_PATH . 'admin/command/Install/install.lock'))
     header("location:./install.php");
     exit;
 }
-
+header("Access-Control-Allow-Origin: *");
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
