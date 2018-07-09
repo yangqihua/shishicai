e013d26d502adb69a41d6e5c98b5dacc6faf24c2
yangqihua <904693433@qq.com>
2018-07-10 00:30:25 +0800

    完成自动下单

--- application/api/controller/Gate.php
+++ application/api/controller/Gate.php
@@ -5,8 +5,10 @@ namespace app\api\controller;
 use app\admin\model\Block;
 use app\admin\model\GateOrder;
 use app\admin\model\Market;
+use app\admin\model\Zhuan;
 use app\common\controller\Api;
 use app\common\library\GateLib;
+use app\common\model\Config;
 use fast\Http;
 
 class Gate extends Api
@@ -30,17 +32,111 @@ class Gate extends Api
         parent::_initialize();
     }
 
-    public function ban_zhuan(){
-        $url = 'https://www.bcex.top/Api_Order/depth';
-        $json = Http::post($url,['symbol'=>'rating2eth']);
-        if(!$json){
-            $json = Http::post($url,['symbol'=>'rating2eth']);
+    public function ban_zhuan()
+    {
+        $data = array_merge($this->get_bcex_ask_bid(), $this->get_gate_ask_bid());
+        if ($data['gate_ask_price']*1.013 < $data['bcex_bid_price']) { // 此时去gate买，去bcex卖
+            $data['remark'] = 'bcex_bid 买方比 gate 卖方多' . ((round($data['bcex_bid_price'] / $data['gate_ask_price'], 4) - 1) * 100) . '%';
+            $data['order_count'] = min($data['gate_ask_count'], $data['bcex_bid_count']);
+//            $this->order_gate_bcex(1, 700, $data['gate_ask_price'], $data['bcex_bid_price']);
+            $this->order_gate_bcex(1, $data['order_count'], $data['gate_ask_price'], $data['bcex_bid_price']);
+        } else if ($data['bcex_ask_price']*1.013 < $data['gate_bid_price']) {
+            $data['remark'] = 'gate_bid 买方比 bcex 卖方多' . ((round($data['gate_bid_price'] / $data['bcex_ask_price'], 4) - 1) * 100) . '%';
+            $data['order_count'] = min($data['bcex_ask_count'], $data['gate_bid_count']);
+//            $this->order_gate_bcex(2, 700, $data['bcex_ask_price'], $data['gate_bid_price']);
+            $this->order_gate_bcex(2, $data['order_count'], $data['bcex_ask_price'], $data['gate_bid_price']);
         }
-        $bcexResult = json_decode($json,true);
-//        return json($bcexResult);
-        $bcexAsks1 = $bcexResult['data']['asks'][count($bcexResult['data']['asks'])-1]; // 卖1
-        $bidsAsks1 = $bcexResult['data']['bids'][0];  // 买1
-        return json(['$bcexAsks1'=>$bcexAsks1,'$bidsAsks1'=>$bidsAsks1]);
+        $zhuanModel = new Zhuan();
+        $zhuanModel->allowField(true)->save($data);
+        return json(['data' => $data]);
+    }
+
+    // type :1 代表gate买，bcex卖，2反之
+    private function order_gate_bcex($type, $count, $buy_price, $sell_price)
+    {
+        if ($count < 500) {
+            trace('下单数量: ' . $count . '小于500，不能下单', 'error');
+            return;
+        }
+        if ($count > 2000) {
+            $count = 2000;
+        }
+        // gate 买，bcex卖
+        if ($type === 1) {
+            $tryCount = 1;
+            $bcexRes = $this->order_bcex('sale', $sell_price, $count);
+            while (!$bcexRes && $tryCount++ < 5) {
+                $bcexRes = $this->order_bcex('sale', $sell_price, $count);
+            }
+            if ($bcexRes) {
+                $gateRes = json_encode($this->gateLib->buy('rating_eth', $buy_price, $count));
+                trace('bcex下单结果：' . $bcexRes . ',gate下单结果：' . $gateRes, 'error');
+            }
+        } else {
+            $tryCount = 1;
+            $bcexRes = $this->order_bcex('buy', $buy_price, $count);
+            while (!$bcexRes && $tryCount++ < 5) {
+                $bcexRes = $this->order_bcex('buy', $buy_price, $count);
+            }
+            if ($bcexRes) {
+                $gateRes = json_encode($this->gateLib->sell('rating_eth', $sell_price, $count));
+                trace('bcex下单结果：' . $bcexRes . ',gate下单结果：' . $gateRes, 'error');
+            }
+        }
+    }
+
+    private function order_bcex($type, $price, $number)
+    {
+        $url = 'https://www.bcex.top/Api_Order/coinTrust';
+        $config = new Config();
+        $keyResult = $config->where("name", "bcex_pub")->find();
+        $bcex_pub = $keyResult['value'];
+        $keyResult = $config->where("name", "bcex_pri")->find();
+        $bcex_pri = $keyResult['value'];
+        $params = [
+            'api_key' => $bcex_pub,
+            'symbol' => 'rating2eth',
+            'type' => $type,
+            'price' => $price,
+            'number' => $number,
+            'sign' => md5('api_key=' . $bcex_pub . '&number=' . $number . '&price=' . $price . '&symbol=rating2eth&type=' . $type . '&secret_key=' . $bcex_pri),
+        ];
+        $result = Http::post($url, $params);
+        return $result;
+    }
+
+    private function get_gate_ask_bid()
+    {
+        $gateResult = $this->gateLib->get_orderbook('rating_eth');
+        $gateAsks1 = $gateResult['asks'][count($gateResult['asks']) - 1]; // 卖1
+        $gateBids1 = $gateResult['bids'][0];  // 买1
+        $gask = $this->num($gateAsks1[0]) . ',' . $gateAsks1[1];
+        $gbid = $this->num($gateBids1[0]) . ',' . $gateBids1[1];
+        return [
+            'gate_ask' => $gask, 'gate_bid' => $gbid,
+            'gate_ask_price' => $this->num($gateAsks1[0]), 'gate_ask_count' => $gateAsks1[1],
+            'gate_bid_price' => $this->num($gateBids1[0]), 'gate_bid_count' => $gateBids1[1],
+        ];
+    }
+
+    private function get_bcex_ask_bid()
+    {
+        $url = 'https://www.bcex.top/Api_Order/depth';
+        $json = Http::post($url, ['symbol' => 'rating2eth']);
+        $tryCount = 1;
+        while (!$json && $tryCount++ < 5) {
+            $json = Http::post($url, ['symbol' => 'rating2eth']);
+        }
+        $bcexResult = json_decode($json, true);
+        $bcexAsks1 = $bcexResult['data']['asks'][count($bcexResult['data']['asks']) - 1]; // 卖1
+        $bcexBids1 = $bcexResult['data']['bids'][0];  // 买1
+        $bask = $this->num($bcexAsks1[0]) . ',' . $bcexAsks1[1];
+        $bbid = $this->num($bcexBids1[0]) . ',' . $bcexBids1[1];
+        return [
+            'bcex_ask' => $bask, 'bcex_bid' => $bbid,
+            'bcex_ask_price' => $this->num($bcexAsks1[0]), 'bcex_ask_count' => $bcexAsks1[1],
+            'bcex_bid_price' => $this->num($bcexBids1[0]), 'bcex_bid_count' => $bcexBids1[1],
+        ];
     }
 
     public function get_ticker()
@@ -144,6 +240,8 @@ class Gate extends Api
         if (false !== stripos($num, "e")) {
             $a = explode("e", strtolower($num));
             return bcmul($a[0], bcpow(10, $a[1], $double), $double);
+        } else {
+            return $num;
         }
     }
 
